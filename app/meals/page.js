78599e866406f24cd2e66443d1e8d674d3a705e8
Ralph Woiwode <rawoiwode@gmail.com>
2024-01-31 15:03:12 -0600

    Creating main meals components and fixing some styling
    Creating the components related to meals
    Fixing styling issue with nav link

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,19 +1,24 @@
 import Link from "next/link";
 import React from "react";
+import styles from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
 
 const MealsPage = () => {
   return (
     <>
-      <h1>Meals</h1>
-      <p>
-        <Link href={"/meals/share"}>Share</Link>
-      </p>
-      <p>
-        <Link href={"/meals/burger"}>Burger</Link>
-      </p>
-      <p>
-        <Link href={"/meals/pasta"}>Pasta</Link>
-      </p>
+      <header className={styles.header}>
+        <h1>
+          Delicious meals, created{" "}
+          <span className={styles.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite recipe and cook it yourself</p>
+        <p className={styles.cta}>
+          <Link href={"/meals/share"}>Share your favorite recipe</Link>
+        </p>
+      </header>
+      <main className={styles.main}>
+        <MealsGrid meals={[]} />
+      </main>
     </>
   );
 };
