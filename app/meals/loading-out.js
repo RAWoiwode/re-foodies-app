6f2fa1eca892d69182f90f82e0aea6617a8b5606
Ralph Woiwode <rawoiwode@gmail.com>
2024-04-03 14:03:38 -0500

    Lowering delay and adding notes

--- app/meals/loading-out.js
+++ app/meals/loading-out.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./loading.module.css";
 
+// Used if you want to cover the entier page with a loading display
 const MealsLoadingPage = () => {
   return <p className={styles.loading}>Fetching meals...</p>;
 };
